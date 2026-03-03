3bbd92f872903956f3dd80695e0165e1a9e65a61
Miguel Barbosa <m270300@dac.unicamp.br>
2026-03-03 14:57:05 -0300

    +

--- TAREFA01/src/App.java
+++ TAREFA01/src/App.java
@@ -1,7 +1,10 @@
+import Bonecos.*;
+import Cartas.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        Personagem personagem = new Personagem();
-        Inimigo inimigo = new Inimigo();
+        Personagem personagem = new Personagem("heroi", 20, 20);
+        Inimigo inimigo = new Inimigo("inimigo", 5, 5);
         Carta carta = new Carta();
         CartaDano cartaDano = new CartaDano();
         CartaEscudo cartaEscudo = new CartaEscudo();
--- TAREFA01/src/Bonecos/Personagem.java
+++ TAREFA01/src/Bonecos/Personagem.java
@@ -1,4 +1,27 @@
 package Bonecos;
 public class Personagem {
+    private String nome;
+    private int vida;
+    private int escudo;
 
+    public Personagem(String nome, int vida, int escudo){
+        this.escudo = escudo;
+        this.nome = nome;
+        this.vida = vida;
+    }
+
+    public int receberDano(int dano){
+        vida -= dano;
+        return vida;
+    }
+
+    public int ganharEscudo(int bonus){
+        escudo+=bonus;
+        return escudo;
+    }
+
+    public int estarVivo (){
+        if (vida == 0) return 0;
+        else return 1;
+    }
 }
