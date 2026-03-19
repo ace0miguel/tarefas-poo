8282b88e0211362dc31010df276edba616bb2e96
Miguel Barbosa <m270300@dac.unicamp.br>
2026-03-19 13:57:14 -0300

    incompleto

--- app/src/main/java/Entidades/Inimigo.java
+++ app/src/main/java/Entidades/Inimigo.java
@@ -1,18 +1,33 @@
 package Entidades;
 
+import java.util.ArrayList;
+import EfeitosDeStatus.Efeito;
+import Cartas.CartaAtaqueComEfeito;
+
 //import Bonecos.Heroi;
 public class Inimigo extends Entidade{
 
     private int dano;
+    private ArrayList<Efeito> sangramentoAcumulado;
 
     public Inimigo(String nome, int vida, int dano){
         super(nome, vida);
         this.dano = dano;
+        this.sangramentoAcumulado = new ArrayList<Efeito>(); // isso aqui é uma lista dos danos de bleeding q o inimigo ainda tem pra tomar a cada turno, pq ele pode ter mais de 1 bleeding
+
     }
+
+//fazer add do efeito(add sangramento) e o sangrar (receber dANO e verificar duracao)
 
     public void atacar(Heroi alvo){
         alvo.receberDano(this.dano);
     }
+
+    public void getSangramento(Efeito sangramento){
+
+    }
+
+    public void sangrar()
 
     @Override
     public String status(){
